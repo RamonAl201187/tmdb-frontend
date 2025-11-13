0d7672c6ad8aea8a6ad7dde090037694fa2f1880
Ramón <al201187@alumnos.uacj.mx>
2025-11-13 09:09:10 -0700

    Update API URL to Render

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Configuración de la API
-const API_BASE_URL = 'http://127.0.0.1:5000';
+// Detectar ambiente automáticamente
+const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://127.0.0.1:5000'
+    : ''https://tmdb-backend-sdqh.onrender.com';
 
 // Estado global
 let genreData = null;
